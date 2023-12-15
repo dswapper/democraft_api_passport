2c00886bc20b2fe9d185a4a567d1dbe7526b3265
drswa <dr.swapper@ya.ru>
2023-12-16 02:45:38 +0300

    add PUT method to /passport

--- services/api/democraft_api/passport_views.py
+++ services/api/democraft_api/passport_views.py
@@ -6,6 +6,7 @@ from flask import request, abort, jsonify, Response
 from random import randint
 from datetime import datetime
 
+# TODO: history of changes (for put and delete)
 
 @app.route('/api/v1/passport', methods=['POST'])
 def post_passport() -> Response:
@@ -82,4 +83,38 @@ def delete_passport_by_number(rp_number: str) -> Response:
     else:
         abort(400)
 
-    return jsonify(status='complited')
+    return jsonify(status='ok')
+
+
+@app.route('/api/v1/passport/by_number/<string:rp_number>', methods=['PUT'])
+def update_passport_by_number(rp_number: str) -> Response:
+    passport = db.session.query(Passport).filter_by(rp_number=rp_number).limit(1).first()
+
+    if passport is None:
+        abort(400)
+
+    if not request.json:
+        abort(400)
+
+    try:
+        new_discord_tag = request.json['new_discord_tag']
+    except KeyError:
+        new_discord_tag = None
+
+    try:
+        new_nickname = request.json['new_nickname']
+    except KeyError:
+        new_nickname = None
+
+    if new_discord_tag is not None:
+        passport.discord_tag = new_discord_tag
+
+    if new_nickname is not None:
+        passport.nickname = new_nickname
+
+    if new_nickname is None and new_discord_tag is None:
+        abort(400)
+
+    db.session.commit()
+
+    return jsonify(status='ok')
